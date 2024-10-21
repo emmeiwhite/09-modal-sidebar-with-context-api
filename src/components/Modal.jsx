42ff0e2a7824da03c0df28176a8a86ee14180609
emmeiwhite <emmeiwhite@gmail.com>
2024-10-21 19:41:34 +0530

    Approaching Modal Logic via CSS

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,20 @@
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from '../context'
+
 export default function Modal() {
-  return <div>Modal</div>
+  const { isModalOpen, closeModal } = useGlobalContext()
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <button
+          className="close-modal-btn"
+          onClick={closeModal}
+        >
+          <FaTimes />
+        </button>
+
+        <h4>The Modal is working fine</h4>
+      </div>
+    </div>
+  )
 }
