8db5ab1fb7f6eeb3a8b1e8c1d759091a30a3e23e
emmeiwhite <emmeiwhite@gmail.com>
2024-10-22 18:42:02 +0530

    Step-1: Create and export the Context

---
+++ src/context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from 'react'
+
+export const GlobalContext = createContext()
+// Now this is our context by the name GlobalContext
+
+// Now we create a Component from which we return GlobalContext.Provider, that's the idea to proceed with
