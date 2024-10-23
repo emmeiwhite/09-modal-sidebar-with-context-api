a61fc3a4c8c45a3ac9b9e8a761d9c26ae2525942
emmeiwhite <emmeiwhite@gmail.com>
2024-10-23 15:26:05 +0530

    mapping over links and social icons data

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,52 @@
+import logo from '../logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { useGlobalContext } from '../context'
+import { social, links } from '../data'
 
 export default function Sidebar() {
   const { closeSidebar, isSidebarOpen } = useGlobalContext()
   return (
-    <div className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
-      <button
-        className="btn close-btn"
-        onClick={closeSidebar}
-      >
-        <FaTimes />
-      </button>
-      <h1>Sidebar!!!</h1>
-    </div>
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <div className="sidebar-header">
+        <img
+          src={logo}
+          alt="Company Logo"
+          className="logo"
+        />
+        <button
+          className="btn close-btn"
+          onClick={closeSidebar}
+        >
+          <FaTimes />
+        </button>
+      </div>
+
+      {/* Menu List | We have social array to construct this in React */}
+
+      <ul className="links">
+        {links.map(link => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+      <ul className="links">
+        {social.map(link => {
+          const { id, url, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
   )
 }
