206324ff4890fe9c16dc54c68f1267194135ee43
emmeiwhite <emmeiwhite@gmail.com>
2024-10-23 12:04:44 +0530

    Step-1: Create the Context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,8 @@
-import { useGlobalContext } from '../context'
 import { FaBars } from 'react-icons/fa'
 
 export default function Home() {
-  const { test, updateTestString } = useGlobalContext()
-
   return (
     <main>
-      {/* <button className="sidebar-toggle">
-        <FaBars />
-      </button> */}
       <button className="btn">open modal</button>
     </main>
   )
---
+++ src/context.jsx
@@ -0,0 +1,4 @@
+// Step-1: Create the Context ( GlobalContext for now)
+import { createContext } from 'react'
+
+const GlobalContext = createContext()
